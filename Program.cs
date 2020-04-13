19e4dccfc52a73c9e6818f4fdfe7719241f3bfd2
Dejavuuu <chris.moye21@gmail.com>
2020-04-14 01:04:23 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //can't figure out why 'Boolean canContinue = false' isn't printing
+            bool canContinue = false;
+            if (canContinue != true)
+               
+            {
+                Console.WriteLine("Proceed...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You Shall Not Pass!");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
