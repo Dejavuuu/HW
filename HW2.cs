1a1d3c1069a68fd84ac156b920f6168d3467b50a
Chris Moye <chris.moye21@gmail.com>
2020-05-03 20:23:39 +0900

    completed project, now committing and pushing

---
+++ HW2.cs
@@ -0,0 +1,61 @@
+/*
+***Homework Assignment 2:
+
+1. Create a console program  (choose .net framework)
+
+2. Create some code in the Main Method() that:
+	a. Shows a console message "sup from while loop" 5 times (using a while loop).
+	b. Shows a console message "sup from for loop" 2 times (using for loop)
+	c. Show a console message "sup once from do-while loop"
+		1. Use a bool variable called runOnce.  Set this to true initially.
+		2. set runOnce = false inside the do-while loop"
+		3. Play around with what happens when you change the initial value of runOnce (toggle between true and false)
+
+
+
+3. Commit and push to GitHub remote repo.
+*/
+
+int begin = 0;
+
+while(begin < 6 ) 
+{
+	begin++;
+	System.Console.WriteLine("sup from while loop");
+}
+
+//
+
+int message = 0;
+
+for (int message = 0; message < 2; message = message + 1) 
+{
+ 
+	System.Console.WriteLine("sup from for loop");
+}
+
+//
+
+bool runOnce = true;
+
+do {
+
+	System.Console.WriteLine("sup once from do-while loop");
+} while(runOnce == false);
+
+while(runOnce)
+
+//
+
+bool runOnce = false;
+
+do {
+
+	System.Console.WriteLine("sup once from do-while loop");
+} while(runOnce == true);
+
+
+
+
+
+
